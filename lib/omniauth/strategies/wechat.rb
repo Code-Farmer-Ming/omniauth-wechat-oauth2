e0f5f7652ad7a3e69c17d6fe52c621501aea3d6c
ming <zhangming1021@gmail.com>
2025-02-05 15:16:54 +0800

    👌 IMPROVE: rename

--- lib/omniauth/strategies/wechat.rb
+++ lib/omniauth/strategies/wechat.rb
@@ -3,7 +3,7 @@ require "omniauth-oauth2"
 module OmniAuth
   module Strategies
     class Wechat < OmniAuth::Strategies::OAuth2
-      option :name, "wechat"
+      option :name, "open_wechat"
 
       option :client_options, {
         site:          "https://api.weixin.qq.com",
